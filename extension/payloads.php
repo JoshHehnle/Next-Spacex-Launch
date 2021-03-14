36c7d73c6371f8f7193690ec9fed70713292f69a
Joshua <35235443+JoshHehnle@users.noreply.github.com>
2021-03-14 21:55:12 +0100

    Update payloads.php

--- extension/payloads.php
+++ extension/payloads.php
@@ -1,14 +1,9 @@
 <?php
-
-$get_url = "https://api.spacexdata.com/v4/payloads/$payload_id"; // get the payloaddata from the api
-
-$string = file_get_contents("$get_url");
-$json_a = json_decode($string, true);
-
-/*All payloads Variables form Json*/
-
-$payload_customer = $json_a['customers']['0'];
-$payload_type = $json_a['type'];
-$payload_orbit = $json_a['regime'];
-
+  $get_url = "https://api.spacexdata.com/v4/payloads/$payload_id"; // get the payloaddata from the api
+  $string = file_get_contents("$get_url");
+  $json_a = json_decode($string, true);
+  /*All payloads Variables form Json*/
+  $payload_customer = $json_a['customers']['0'];
+  $payload_type = $json_a['type'];
+  $payload_orbit = $json_a['regime'];
 ?>
